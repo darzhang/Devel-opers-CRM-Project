a7540a3aeb579fa78009080694a555cea7c4f4ef
Erdinan <angkajayae@student.unimelb.edu.au>
2021-08-22 17:05:39 +1000

    add dpt and org model

---
+++ controllers/departmentController.js
@@ -0,0 +1,55 @@
+const Department = require("../models/department");
+
+module.exports = {
+    createOne: async (req, res) => {
+        try {
+            let department = new Department(req.body)
+            await department.save(function(err) {
+                if (err) return res.status(500).json(err)
+                res.json(department)
+            })
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    },
+    deleteOne: async (req, res) => {
+        try {
+            await Department.findOneAndRemove({ name: req.params.name }, function (err) {
+                if (err) return res.status(400).json(err);
+                res.status(204).json({});
+            });
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    },
+    getOne: async (req, res) => {
+        try {
+            await Department.find({ name: req.params.name})
+            .exec(function (err, department) {
+                if (err) return res.status(404).json(err);
+                res.json(department);
+            });
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    },
+    getAll: async (req, res) => {
+        try {
+            await Department.find({})
+            .exec(function (err, department) {
+                if (err) return res.status(404).json(err);
+                res.json(department);
+            })
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    }
+}
---
+++ controllers/organisationController.js
@@ -0,0 +1,55 @@
+const Organisation = require("../models/organisation");
+
+module.exports = {
+    createOne: async (req, res) => {
+        try {
+            let organisation = new Organisation(req.body)
+            await organisation.save(function(err) {
+                if (err) return res.status(500).json(err)
+                res.json(organisation)
+            })
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    },
+    deleteOne: async (req, res) => {
+        try {
+            await Organisation.findOneAndRemove({ name: req.params.name }, function (err) {
+                if (err) return res.status(400).json(err);
+                res.status(204).json({});
+            });
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    },
+    getOne: async (req, res) => {
+        try {
+            await Organisation.find({ name: req.params.name})
+            .exec(function (err, org) {
+                if (err) return res.status(404).json(err);
+                res.json(org);
+            });
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    },
+    getAll: async (req, res) => {
+        try {
+            await Organisation.find({})
+            .exec(function (err, org) {
+                if (err) return res.status(404).json(err);
+                res.json(org);
+            })
+        }
+        catch(err){
+            res.status(400);
+            return res.send("Database query failed");
+        }
+    }
+}
