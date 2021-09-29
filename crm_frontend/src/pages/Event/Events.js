baeb0e9312cd2d597f9d807f7b98600d44d7d861
Darren Zhang <darzhang@student.unimelb.edu.au>
2021-09-29 14:12:14 +0700

    fix the data grid layout for the events page

--- crm_frontend/src/pages/Event/Events.js
+++ crm_frontend/src/pages/Event/Events.js
@@ -5,6 +5,12 @@ import EventHeader from '../../components/Event/EventHeader'
 import moment from 'moment'
 import DataGridComp from '../../components/Event/DataGridComp'
 import swal from 'sweetalert'
+import CircularProgress from '@mui/material/CircularProgress';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { Link } from 'react-router-dom';
+import Button from '../../components/Event/Button'
 
 
 const Events = () => {
@@ -22,9 +28,22 @@ const Events = () => {
     dateAdded: new Date()
   }
 
+  const compareDate = (first, second) => {
+    if(first.startTime < second.startTime){
+      return -1
+    }else if (first.startTime === second.startTime){
+      return 0
+    }else {
+      return 1
+    }
+  }
+
   useEffect(() => {
     const getEvents = async () =>{
       const eventsFromBackEnd = await fetchEvents()
+      //sort based on startTime
+      eventsFromBackEnd.sort(compareDate)
+
       //preprocessing data for displaying in grid
       eventsFromBackEnd.forEach((event) => {
         event.id = event._id; 
@@ -34,7 +53,6 @@ const Events = () => {
         event.dateAdded = moment(event.dateAdded).format(timeFormat);
         event.participants = event.participants.map((participant)=>participant.name).join(", ")
       })
-      console.log(eventsFromBackEnd)
       setEvents(eventsFromBackEnd)
     }
 
@@ -112,12 +130,54 @@ const Events = () => {
     
   }
 
+  const showDetailColumn = {
+    width: 120,
+    field:'showDetail',
+    headerName: 'Detail',
+    filterable:false,
+    renderCell: (cellValues) => {
+      return (
+        <Link to={`/event/${cellValues.row.id}`}>
+        <Button color={'blue'} text={'Detail'} onClick={null}/>
+        </Link>
+      );
+    }
+  }
+
+  const deleteColumn = {
+    width: 120,
+    filterable: false,
+    field:'deleteEvent',
+    headerName: 'Delete',
+    renderCell: (cellValues) => {
+      return (
+        <Tooltip title="Delete Event">
+          <IconButton onClick={() => deleteEvent(cellValues.row.id)}>
+            <DeleteIcon/>
+          </IconButton>
+        </Tooltip>
+      )
+    }
+  }
+
+  const columns =[
+    { field: 'eventName', headerName: 'Event Name', minWidth: 160, flex: 1},
+    { field: 'startTime', headerName: 'Start Time', minWidth: 160, flex: 1, filterable:false},
+    { field: 'endTime', headerName: 'End Time', minWidth: 160, flex: 1, filterable:false},
+    // { field: 'description', headerName: 'Description', minWidth: 160, flex: 1, hide: true},
+    { field: 'location', headerName: 'Location', minWidth: 160, flex: 1},
+    { field: 'participants', headerName: 'Participants', minWidth: 160, flex: 1},
+    // { field: 'dateAdded', headerName: 'Date Added', minWidth: 180, flex: 1, hide: true},
+    showDetailColumn,
+    deleteColumn
+  ];
+
   return (
     <div className="Events" style={{marginLeft:"75px"}}>
       <EventHeader onAdd ={() => setShowAddEvent(!showAddEvent)} color={showAddEvent ? 'red' : 'blue'} text={showAddEvent ? 'Close' : 'Add'}/>
       {showAddEvent && <AddEvent event={defaultEvent} onEdit={null} onAdd={addEvent} closeForm ={() => setShowAddEvent(false)} text={'Add Event'}/>}
       <div className="listOfEvents">
-        <DataGridComp events={events} onDelete={(id) => deleteEvent(id)}></DataGridComp>
+        {events.length > 0 ? <DataGridComp events={events} columns={columns}></DataGridComp> : <CircularProgress />}
       </div>
       
     </div>
