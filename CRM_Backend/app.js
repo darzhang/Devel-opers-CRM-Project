f65bee86f52adbdca47e127f901ae06aa61dbd49
Erdinan Angkajaya <angkajayae@student.unimelb.edu.au>
2021-10-18 21:23:18 +1100

    minor error

--- CRM_Backend/app.js
+++ CRM_Backend/app.js
@@ -45,7 +45,7 @@ app.engine('hbs', exphbs({
 app.use(express.json())
 app.use(cors({
   credentials: true,
-  origin: "https://developer-crm-frontend.herokuapp.com",
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
